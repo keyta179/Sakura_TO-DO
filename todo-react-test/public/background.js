f6dddf6dccebfa2d7c0d180e6fe928112cc1f333
fukuchy <taichita120@mail.com>
2024-04-27 12:54:15 +0900

    changed item border

--- todo-react-test/public/background.js
+++ todo-react-test/public/background.js
@@ -1,5 +1,5 @@
 chrome.webNavigation.onCompleted.addListener(function(details) {
-    if (details.url.includes("youtube.com")) {
+    if (details.url.includes("youtube.com") || details.url.includes("twitter.com")) {
       console.log("youtube.comにアクセスしました。アラートを表示します。");
       chrome.scripting.executeScript({
           target: {tabId: details.tabId},
