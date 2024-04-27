00e3c079397408e13d918463eb5a8472daffaf33
fukuchy <taichita120@mail.com>
2024-04-27 14:06:07 +0900

    fix alert.js

--- todo-react-test/public/alert.js
+++ todo-react-test/public/alert.js
@@ -1,5 +1,5 @@
-let nowDate = new Date().toLocaleString().split(" ");
 chrome.storage.local.get("flagDate", function (result) {
+    let nowDate = new Date().toLocaleString().split(" ");
     let lastDate = result.flagDate;
     if (!lastDate || lastDate !== nowDate[0]) {
         alert('やらなきゃいけないことは終わったの?');
